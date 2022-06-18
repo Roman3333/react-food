10f9476bfe73bcab2bf23b92a84883647147cbb2
Roman <rosipenko@yandex.ru>
2022-06-18 23:36:48 +0500

    suspence/loadable

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Loadable from 'react-loadable';
 
 import Header from './components/Header';
 import Home from './pages/Home';
-import Basket from './pages/Basket';
-import NotFound from './pages/NotFound';
-import PizzaPage from './pages/PizzaPage';
-
 import './scss/app.scss';
+
+const Basket = React.lazy(() => import(/* webpackChunkName: "Basket" */ './pages/Basket'));
+const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'));
+const PizzaPage = Loadable({
+  loader: () => import(/* webpackChunkName: "FullPizzaЗфпу" */ './pages/PizzaPage'),
+  loading: () => <div>Загрузка...</div>,
+});
 
 function App() {
   return (
@@ -14,10 +19,24 @@ function App() {
       <Header />
       <div className="content">
         <Routes>
-          <Route path="/pizza-react" element={<Home />} />
-          <Route path="/basket" element={<Basket />} />
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/basket"
+            element={
+              <Suspense fallback={<div>Загрузка корзины</div>}>
+                <Basket />
+              </Suspense>
+            }
+          />
           <Route path="/pizza/:id" element={<PizzaPage />} />
-          <Route path="*" element={<NotFound />} />
+          <Route
+            path="*"
+            element={
+              <Suspense fallback={<div>Загрузка...</div>}>
+                <NotFound />
+              </Suspense>
+            }
+          />
         </Routes>
       </div>
     </div>
